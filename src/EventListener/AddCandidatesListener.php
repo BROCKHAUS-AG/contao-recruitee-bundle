a8dbd581b7dae5915084a500f5c30faeb6e6ca96
INoTime <niklas.lurse@icloud.com>
2021-12-03 12:54:37 +0100

    started with adding hooks

---
+++ src/EventListener/AddCandidatesListener.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Microsoft SSO Bundle.
+ *
+ * (c) BROCKHAUS AG 2021 <info@brockhaus-ag.de>
+ * Author Niklas Lurse (INoTime) <nlurse@brockhaus-ag.de>
+ *
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/BROCKHAUS-AG/contao-microsoft-sso-bundle
+ */
+
+namespace BrockhausAg\ContaoRecruiteeBundle\EventListener;
+
+use BrockhausAg\ContaoRecruiteeBundle\Logic\AddCandidatesLogic;
+use Contao\CoreBundle\ServiceAnnotation\Hook;
+use Contao\Form;
+
+class AddCandidatesListener
+{
+    private AddCandidatesLogic $_addCandidatesLogic;
+
+    public function __construct()
+    {
+        $this->_addCandidatesLogic = new AddCandidatesLogic();
+    }
+
+    /**
+     * @Hook("processFormData")
+     */
+    public function onAddCandidate(array $submittedData,
+                                   array $formData,
+                                   ?array $files,
+                                   array $labels,
+                                   Form $form)
+    {
+        $this->_addCandidatesLogic->addCandidate($submittedData, $formData, $files, $labels, $form);
+    }
+}
---
+++ src/Logic/AddCandidatesLogic.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Microsoft SSO Bundle.
+ *
+ * (c) BROCKHAUS AG 2021 <info@brockhaus-ag.de>
+ * Author Niklas Lurse (INoTime) <nlurse@brockhaus-ag.de>
+ *
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/BROCKHAUS-AG/contao-microsoft-sso-bundle
+ */
+
+namespace BrockhausAg\ContaoRecruiteeBundle\Logic;
+
+
+use Contao\Form;
+
+class AddCandidatesLogic
+{
+
+    public function __construct() {}
+
+
+    public function addCandidate(array $submittedData, array $formData, ?array $files, array $labels, Form $form) : void
+    {
+
+    }
+}
