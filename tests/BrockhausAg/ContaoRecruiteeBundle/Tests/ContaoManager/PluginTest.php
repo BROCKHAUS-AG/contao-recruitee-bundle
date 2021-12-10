3056b3fe0dbef29fa1830b1b01f50703d94b2439
INoTime <niklas.lurse@icloud.com>
2021-12-10 10:34:13 +0100

    refactored

--- tests/BrockhausAg/ContaoRecruiteeBundle/Tests/ContaoManager/PluginTest.php
+++ tests/BrockhausAg/ContaoRecruiteeBundle/Tests/ContaoManager/PluginTest.php
@@ -1,15 +1,14 @@
 <?php
 
+
 /*
- * This file is part of Contao Microsoft SSO Bundle.
- * 
- * (c) BROCKHAUS AG 2021 <info@brockhaus-ag.de>
- * Author Niklas Lurse (INoTime) <nlurse@brockhaus-ag.de>
+ * This file is part of Contao Recruitee Bundle.
  *
+ * (c) BROCKHAUS AG 2021 <info@brockhaus-ag.de>
  * @license GPL-3.0-or-later
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/BROCKHAUS-AG/contao-microsoft-sso-bundle
+ * @link https://github.com/brockhaus-ag/contao-recruitee-bundle
  */
 
 declare(strict_types=1);
