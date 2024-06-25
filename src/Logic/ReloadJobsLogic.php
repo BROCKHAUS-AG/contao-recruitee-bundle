18fe81f5a1a78f405f5c68a3616804bd51000e37
Tom Temme <ttemme@brockhaus-ag.de>
2024-06-25 17:45:42 +0200

    implemented location implementation

--- src/Logic/ReloadJobsLogic.php
+++ src/Logic/ReloadJobsLogic.php
@@ -20,8 +20,10 @@ use Psr\Log\LoggerInterface;
 
 
 DEFINE("RECRUITEE_URL", "https://api.recruitee.com");
-DEFINE("OFFERS_URL", RECRUITEE_URL."/c/%s/offers");
-DEFINE("OFFERS_BY_ID_URL", OFFERS_URL. "/%d");
+DEFINE("OFFERS_URL", RECRUITEE_URL . "/c/%s/offers");
+DEFINE("OFFERS_BY_ID_URL", OFFERS_URL . "/%d");
+DEFINE("LOCATIONS_URL", RECRUITEE_URL . "/locations");
+DEFINE("LOCATIONS_BY_ID_URL", LOCATIONS_URL . "/%d");
 
 class ReloadJobsLogic
 {
@@ -42,12 +44,12 @@ class ReloadJobsLogic
         $this->locations = $this->_ioLogic->loadRecruiteeConfigLocations();
     }
 
-    private function createJob(array $job, array $offer, string $category) : ?array
+    private function createJob(array $job, array $offer, string $category): ?array
     {
         $jobDescription = $offer["description"];
         $requirements = $offer["requirements"];
 
-        if($job["status"] == "published") {
+        if ($job["status"] == "published") {
             return array(
                 'stellenname' => $job['title'],
                 'id' => $job['id'],
@@ -66,13 +68,14 @@ class ReloadJobsLogic
         return null;
     }
 
-    private function getJob(string $companyIdentifier, string $bearerToken, string $category) : array
+    private function getJob(string $companyIdentifier, string $bearerToken, string $category): array
     {
         $jobs = $this->getJobsFromApi($companyIdentifier, $bearerToken);
         $jobDescriptions = array();
         if ($jobs) {
-            foreach($jobs["offers"] as $job) {
+            foreach ($jobs["offers"] as $job) {
                 $offer = $this->getOfferFromApiById($job["id"], $bearerToken, $companyIdentifier)["offer"];
+                $job['einsatzort'] = $this->getLocationWithIdFromApi($companyIdentifier, $bearerToken, 0);
                 $jobDescription = $this->createJob($job, $offer, $category);
                 if ($jobDescription) {
                     array_push($jobDescriptions, $jobDescription);
@@ -82,33 +85,44 @@ class ReloadJobsLogic
         return $jobDescriptions;
     }
 
-    private function getJobs() : array
+    private function getJobs(): array
     {
         $jobs = array();
         foreach ($this->locations as $location) {
             $job = $this->getJob($location["companyIdentifier"], $location["bearerToken"], $location["category"]);
-            array_push( $jobs, $job);
+            array_push($jobs, $job);
         }
         return $jobs;
     }
 
-    private function createOffersUrlWithCompanyId(string $companyId) : string
+    private function createOffersUrlWithCompanyId(string $companyId): string
     {
         return sprintf(OFFERS_URL, $companyId);
     }
 
-    private function createOffersURLWithCompanyIdAndOfferId(string $companyId, int $offerId) : string
+    private function createLocationsUrlWithCompanyIdAndLocationId(string $companyId, int $locationId):string
+    {
+        return sprintf(LOCATIONS_URL, $companyId);
+    }
+
+    private function createOffersURLWithCompanyIdAndOfferId(string $companyId, int $offerId): string
     {
         return sprintf(OFFERS_BY_ID_URL, $companyId, $offerId);
     }
 
-    private function getJobsFromApi(string $companyId, string $bearerToken) : ?array
+    private function getJobsFromApi(string $companyId, string $bearerToken): ?array
     {
         $offersUrl = $this->createOffersUrlWithCompanyId($companyId);
         return $this->_httpLogic->httpGetWithBearerToken($offersUrl, $bearerToken);
     }
 
-    private function getOfferFromApiById(int $offerId, string $bearerToken, string $companyId) : ?array
+    private function getLocationWithIdFromApi(string $companyId, string $bearerToken, int $locationId): ?array
+    {
+        $locationsUrlWithId = $this->createLocationsUrlWithCompanyIdAndLocationId($companyId, $locationId);
+        return $this->_httpLogic->httpGetWithBearerToken($locationsUrlWithId, $bearerToken);
+    }
+
+    private function getOfferFromApiById(int $offerId, string $bearerToken, string $companyId): ?array
     {
         $offersUrl = $this->createOffersURLWithCompanyIdAndOfferId($companyId, $offerId);
         return $this->_httpLogic->httpGetWithBearerToken($offersUrl, $bearerToken);
@@ -119,7 +133,7 @@ class ReloadJobsLogic
         $this->_ioLogic->saveJobsToFile($jobs);
     }
 
-    public function loadJobs() : Response
+    public function loadJobs(): Response
     {
         $jobs = $this->getJobs();
         $this->saveJobs($jobs);
